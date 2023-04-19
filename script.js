e0d43b4f8c3a8681d11744cfd3f7d1e1884fa8fa
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-04-19 12:58:49 +0200

    Añadida funcionalidad de Menu de Navegación desplegable. Cambios menores de maquetación del <header>

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+
+
+function navMenu(){
+    let navLinks = document.querySelector(".navLinks");
+    let mainLogo = document.querySelector(".mainLogo");
+    let searchBar = document.querySelector(".searchBar");
+
+    if(navLinks.style.display === "flex"){
+        navLinks.style.display = "none";
+    }else {
+        navLinks.style.display = "flex";
+        navLinks.style.flexDirection = "column";
+    }
+    
+    /* navLinks.forEach(element => {
+        element.style.display = "none"
+    }); */
+}
+
+
+
 window.onload = () =>{
+
+    let navButton = document.querySelector(".navButton");
+    navButton.addEventListener("click", navMenu);
     
     // Para cargar id wrapper por codigo
     //let wrapper = document.querySelector("body").children[0];
@@ -6,3 +30,14 @@ window.onload = () =>{
 
 
 }
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon 
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  */
