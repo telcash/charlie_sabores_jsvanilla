60d9d72ea6b554f42e8aaa72639ed4a8c4172097
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-04-19 10:17:00 +0200

    Diagramacion inicial de index.html para moviles

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = () =>{
+    
+    // Para cargar id wrapper por codigo
+    //let wrapper = document.querySelector("body").children[0];
+    //wrapper.setAttribute("id","wrapper");
+
+
+}
