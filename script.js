96b8a03c63bc20dfada0e6c77d9b539c3eebe717
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-04-22 00:16:43 +0200

    Slider rueda, falta acomodar flechas, funcionalidad flechas y estilos

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let activeSlide = 0;
+
+window.onload = () =>{
+
+    let navButton = document.querySelector(".navButton");
+    navButton.addEventListener("click", navMenu);
+    mainSlider();
+}
 
 function navMenu(){
     let navLinks = document.querySelector(".navLinks");
@@ -12,9 +20,18 @@ function navMenu(){
 
 }
 
-window.onload = () =>{
-
-    let navButton = document.querySelector(".navButton");
-    navButton.addEventListener("click", navMenu);
-
+function mainSlider(){
+    let slides = document.getElementsByClassName("slide");
+    for(i=0;i<slides.length;i++){
+        if(i===activeSlide){
+            slides[i].style.display = "block";
+        } else {
+            slides[i].style.display = "none";
+        }
+    }
+    activeSlide++;
+    if(activeSlide===slides.length) {
+        activeSlide = 0;
+    }
+    setTimeout(mainSlider,3000);
 }
