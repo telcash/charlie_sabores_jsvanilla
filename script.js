ff7af4051b4b53644d49939311acfec03e004a56
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2023-04-22 11:47:23 +0200

    Slider con funcionalidad auto y botones prev y next, falta ajustar estilos

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let activeSlide = 0;
+let slides;
+let timeoutId;
 
 window.onload = () =>{
-
+    slides = document.getElementsByClassName("slide");
     let navButton = document.querySelector(".navButton");
     navButton.addEventListener("click", navMenu);
-    mainSlider();
+    
+    showSlide(activeSlide);
+    
+    let arrowLeft = document.querySelector(".arrowLeft");
+    arrowLeft.addEventListener("click", showPrevSlide);
+    let arrowRight = document.querySelector(".arrowRight");
+    arrowRight.addEventListener("click", showNextSlide);
 }
 
 function navMenu(){
@@ -20,18 +28,35 @@ function navMenu(){
 
 }
 
-function mainSlider(){
-    let slides = document.getElementsByClassName("slide");
+function showSlide(slideIndex){
+
+    clearTimeout(timeoutId);
+
     for(i=0;i<slides.length;i++){
-        if(i===activeSlide){
+        if(i===slideIndex){
             slides[i].style.display = "block";
         } else {
             slides[i].style.display = "none";
         }
     }
+    
+    timeoutId = setTimeout(showNextSlide, 5000)
+
+}
+
+function showNextSlide() {
+
     activeSlide++;
-    if(activeSlide===slides.length) {
+    if(activeSlide >= slides.length){
         activeSlide = 0;
     }
-    setTimeout(mainSlider,3000);
+    showSlide(activeSlide);
+}
+
+function showPrevSlide(){
+    activeSlide--;
+    if(activeSlide < 0){
+        activeSlide = (slides.length) - 1;
+    }
+    showSlide(activeSlide);
 }
